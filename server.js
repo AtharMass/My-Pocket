2d7ab13d14fc136f7b9d2df2ca5f79ffd36f28da
Atar Masarwa Saadi <athar@irestweb.com>
2021-06-06 09:25:07 +0300

    Initial Setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const path = require('path')
+var express = require('express')
+var mongoose = require('mongoose')
+const api = require('./server/routes/api')
+
+var app = express()
+const PORT = 3555
+
+mongoose.connect("mongodb://localhost/transactions")
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+
+app.use('/', api)
+
+
+/*=====================================================
+Start the server:
+=======================================================*/
+
+app.listen(PORT, function() {
+    console.log(`Server up and running on port ${PORT}`)
+})
+  
